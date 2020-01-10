875ca12334847419f54b103f8708801e516b6cfe
PawelKowalczuk00 <german4050@wp.pl>
2020-01-10 19:51:18 +0100

    Updates

---
+++ precache-manifest.1d1b8627839d860c912a91a6a90a29f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8713edca7467ff85bf90f0ed3084bb8",
+    "url": "/EscapeRoomFrontend/index.html"
+  },
+  {
+    "revision": "4ca234ab1ec4371b3d95",
+    "url": "/EscapeRoomFrontend/static/css/2.a1f70397.chunk.css"
+  },
+  {
+    "revision": "4f2d25082a1df2d1108e",
+    "url": "/EscapeRoomFrontend/static/css/main.18a2fe35.chunk.css"
+  },
+  {
+    "revision": "4ca234ab1ec4371b3d95",
+    "url": "/EscapeRoomFrontend/static/js/2.60ea7230.chunk.js"
+  },
+  {
+    "revision": "96048aae071b08e3a0f8ca33750b98c0",
+    "url": "/EscapeRoomFrontend/static/js/2.60ea7230.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4f2d25082a1df2d1108e",
+    "url": "/EscapeRoomFrontend/static/js/main.6672dfcf.chunk.js"
+  },
+  {
+    "revision": "39ad9c0c95216ac9b2c7",
+    "url": "/EscapeRoomFrontend/static/js/runtime-main.ab406525.js"
+  },
+  {
+    "revision": "d5bd2677408c9966bb3b30221287b69e",
+    "url": "/EscapeRoomFrontend/static/media/logo.d5bd2677.png"
+  }
+]);
